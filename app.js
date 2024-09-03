49c39109c44e1d86c78e4afb0cc9b678779ed9a4
Juanchopk <juandavidguapo8@gmail.com>
2024-09-03 16:53:49 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,81 @@
+'use estricto'
+/*
+document.write("<h1>Hello world</h1>")
+//Tipos de datos
+//cuerda
+"Hola Papus"
+'Hola Papus'
+//Número
+10000000
+2.35.
+//Booleano
+falso
+cierto
+//Objeto
+'Teodoro Momazos'
+99.9999
+45
+//Variables
+var nombre = "Julio";
+deja el apellido = "Guerrero";
+nombre = "Momazos";
+const PI = 3.14;
+console.log(nombre)
+console.log(apellido)
+consola.log(PI)
+//Camelcase
+deja que userName = "ABB";
+var lastNameOfPerson = "Papus";
+//Operados
+deje NumberOne = 3;
+deja que NumberTwo = 4;
+dejar resultado = NumberOne + NumberTwo;
+console.log(resultado);
+dejemos que namee = 'Gabriel';
+deja que lastName = 'Gonzales';
+let resultt = namee + lastName;
+console.log(resultado);
+deje contraseña = 'Notoiletsigma'
+deje entrar = 'Notoiletsigm'
+deja que resulttt = ingrese == contraseña;
+console.log(resulttt)
+si (resulttt == true) {
+    console.log('Iniciar sesión Correcto')
+} más {
+    console.log('Iniciar sesión Incorrecto')
+}
+deje anotar = 10;
+si (puntuación > 5) {
+    console.log('Eso Papu')
+} más {
+    console.log('Pailas Papu')
+}
+deja que typeCard = 'Debo Card'
+switch (typeCard) {
+    caso 'BancoBogota Card':
+    console.log('Eso papu')
+    romper;
+    caso 'Tarjeta de Débito':
+    console.log('Vamos papu')
+    romper;
+    predeterminado:
+        console.log('Qué está usando')
+        romper;
+}
+//Iteradores
+deje contar = 5;
+mientras (contar < 40) {
+    console.log(conteo)
+    conde++;
+}
+deje que los nombres = ['JhonyKing', 'JhonaPan', 'Garu'];
+console.log(nombres.length)
+for(let i = 0; i < names.length; i++){
+console.log(nombres[i]);
+}
+//Funciones
+función saludo(){
+    console.log('Helle')
+}
+saludo();
+*/
